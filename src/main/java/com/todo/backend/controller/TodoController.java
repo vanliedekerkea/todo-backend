3195131b6e21444da0c208324e5706ebcbe27d92
arno.van.liedekerke <arno.van.liedekerke@accenture.com>
2018-03-08 07:17:31 +0100

    my second commit

---
+++ src/main/java/com/todo/backend/controller/TodoController.java
@@ -0,0 +1,75 @@
+package com.todo.backend.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import com.todo.backend.model.Todo;
+import com.todo.backend.repository.TodoRepository;
+
+import javax.validation.Valid;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api")
+public class TodoController {
+
+    @Autowired
+    TodoRepository todoRepository;
+
+    // Get All Notes
+    @GetMapping("/todos")
+    public List<Todo> getAllTodos() {
+    	return todoRepository.findAll();
+    }
+    
+    // Create a new Note
+    @PostMapping("/todos")
+    public Todo createTodo(@Valid @RequestBody Todo todo) {
+    	return todoRepository.save(todo);
+    }
+    
+    // Get a Single Note
+    @GetMapping("/todos/{id}")
+    public ResponseEntity<Todo> getTodoById(@PathVariable(value = "id") Long todoId) {
+    	Optional<Todo> todo = todoRepository.findById(todoId);
+    	if(!todo.isPresent()) {
+    		return ResponseEntity.notFound().build();
+    	}
+    	return ResponseEntity.ok().body(todo.get());
+    }
+    
+    // Update a Note
+    @PutMapping("/todos/{id}")
+    public ResponseEntity<Todo> updateTodo(@PathVariable(value="id") Long todoId,
+    		@Valid @RequestBody Todo todoDetails) {
+    	Optional<Todo> todo = todoRepository.findById(todoId);
+    	if(!todo.isPresent()) {
+    		return ResponseEntity.notFound().build();
+    	}
+    	todo.get().setTitle(todoDetails.getTitle());
+    	todo.get().setContent(todoDetails.getContent());
+    	
+    	Todo updatedTodo = todoRepository.save(todo.get());
+    	return ResponseEntity.ok(updatedTodo);
+    }
+    
+    // Delete a Note		
+    @DeleteMapping("/todos/{id}")
+    public ResponseEntity<Todo> deleteTodo(@PathVariable(value = "id") Long todoId) {
+    	Optional<Todo> todo = todoRepository.findById(todoId);
+    	if(!todo.isPresent()) {
+    		return ResponseEntity.notFound().build();
+    	}
+    	todoRepository.delete(todo.get());
+    	return ResponseEntity.ok().build();
+    }
+    
+    
+    
+}
+
+
+
+
